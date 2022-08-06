e2d6e2b062f2a8366a8d88e98f9aaa16df7621e6
janep <bp5421@srmist.edu.in>
2022-08-06 15:58:32 +0530

    Added Hosted Challenges

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:ardent_sports/BadmintonSpotSelection.dart';
 import 'package:ardent_sports/HomePage.dart';
+import 'package:ardent_sports/HostedChallenges.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:flutter/material.dart';
 import 'CreateChallenge.dart';
@@ -27,8 +28,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark(),
       initialRoute: '/',
       routes: {
-        '/': (context) => login(),
-        '/homePage': (context) => HomePage(),
+        '/': (context) => HostedChallenges(),
+        '/homePage': (context) => HostedChallenges(),
       },
     );
   }
