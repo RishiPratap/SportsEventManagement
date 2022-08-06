139c70aabc3f38502f558d3b017373f0d893b030
janep <bp5421@srmist.edu.in>
2022-08-06 23:29:14 +0530

    CricketTeamDetailsInput UI Has been made responsive

--- lib/CricketTeamDetailsInput.dart
+++ lib/CricketTeamDetailsInput.dart
@@ -28,9 +28,11 @@ class _CricketTeamDetasilsInputState extends State<CricketTeamDetasilsInput> {
   List<String> teamA_Players = [];
   List<String> teamB_Players = [];
   List<Container> buildPlayers(int count, String team_nam) {
+    double deviceWidth = MediaQuery.of(context).size.width;
+    double deviceHeight = MediaQuery.of(context).size.height;
     List<Container> totalPlayers = [];
     var team_name = Container(
-      margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+      margin: EdgeInsets.fromLTRB(0, deviceWidth * 0.04, 0, 0),
       child: Center(
         child: Text(
           "$team_nam Add Players",
@@ -42,11 +44,12 @@ class _CricketTeamDetasilsInputState extends State<CricketTeamDetasilsInput> {
     for (int i = 1; i <= count; i++) {
       var newPlayer = Container(
         width: MediaQuery.of(context).size.width,
-        height: 50,
+        height: deviceWidth * 0.15,
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
+            borderRadius: BorderRadius.circular(deviceWidth * 0.02),
             color: Colors.black.withOpacity(0.4)),
-        margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        margin: EdgeInsets.fromLTRB(deviceWidth * 0.02, deviceWidth * 0.02,
+            deviceWidth * 0.02, deviceWidth * 0.02),
         child: TextField(
           onChanged: (text) {
             if (team_name == "Team A") {
@@ -56,15 +59,16 @@ class _CricketTeamDetasilsInputState extends State<CricketTeamDetasilsInput> {
             }
           },
           decoration: InputDecoration(
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(deviceWidth * 0.02)),
             hintText: 'Player $i',
             hintStyle: TextStyle(color: Colors.white.withOpacity(0.5)),
             enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
+              borderRadius: BorderRadius.circular(deviceWidth * 0.02),
               borderSide: BorderSide(color: Colors.black.withOpacity(0.001)),
             ),
             focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
+              borderRadius: BorderRadius.circular(deviceWidth * 0.02),
               borderSide: BorderSide(
                 color: Colors.black.withOpacity(0.001),
               ),
@@ -77,8 +81,9 @@ class _CricketTeamDetasilsInputState extends State<CricketTeamDetasilsInput> {
     }
     var submit = Container(
       width: MediaQuery.of(context).size.width,
-      height: 45,
-      margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      height: deviceWidth * 0.1,
+      margin: EdgeInsets.fromLTRB(deviceHeight * 0.02, deviceHeight * 0.02,
+          deviceWidth * 0.02, deviceWidth * 0.02),
       child: RaisedButton(
         onPressed: () {
           ScaffoldMessenger.of(context).showSnackBar(
@@ -90,7 +95,8 @@ class _CricketTeamDetasilsInputState extends State<CricketTeamDetasilsInput> {
         },
         child: Text("Submit"),
         color: Color(0xffD15858),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(deviceWidth * 0.02)),
       ),
     );
     totalPlayers.add(submit);
@@ -100,6 +106,8 @@ class _CricketTeamDetasilsInputState extends State<CricketTeamDetasilsInput> {
 
   @override
   Widget build(BuildContext context) {
+    double deviceWidth = MediaQuery.of(context).size.width;
+    double deviceHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: SafeArea(
@@ -117,8 +125,8 @@ class _CricketTeamDetasilsInputState extends State<CricketTeamDetasilsInput> {
                   Expanded(
                     flex: 1,
                     child: Container(
-                      width: 90,
-                      height: 50,
+                      width: deviceWidth * 0.18,
+                      height: deviceWidth * 0.1,
                       decoration: BoxDecoration(
                           image: DecorationImage(
                               image: AssetImage("assets/AARDENT_LOGO.png"),
@@ -128,8 +136,8 @@ class _CricketTeamDetasilsInputState extends State<CricketTeamDetasilsInput> {
                   Expanded(
                     flex: 1,
                     child: Container(
-                      width: 130,
-                      height: 40,
+                      width: deviceWidth * 0.26,
+                      height: deviceWidth * 0.08,
                       decoration: BoxDecoration(
                           image: DecorationImage(
                               image: AssetImage("assets/Ardent_Sport_Text.png"),
@@ -152,8 +160,8 @@ class _CricketTeamDetasilsInputState extends State<CricketTeamDetasilsInput> {
                   Expanded(
                     flex: 1,
                     child: Container(
-                      width: 40,
-                      height: 40,
+                      width: deviceWidth * 0.08,
+                      height: deviceWidth * 0.08,
                       decoration: BoxDecoration(
                           image: DecorationImage(
                               image: AssetImage("assets/Profile_Image.png"),
@@ -169,8 +177,8 @@ class _CricketTeamDetasilsInputState extends State<CricketTeamDetasilsInput> {
                   Expanded(
                     flex: 1,
                     child: Container(
-                      width: 40,
-                      height: 40,
+                      width: deviceWidth * 0.08,
+                      height: deviceWidth * 0.08,
                       decoration: BoxDecoration(
                           image: DecorationImage(
                               image: AssetImage("assets/money_bag.png"),
@@ -184,7 +192,7 @@ class _CricketTeamDetasilsInputState extends State<CricketTeamDetasilsInput> {
                   Expanded(
                     flex: 1,
                     child: Container(
-                      margin: EdgeInsets.only(left: 15),
+                      margin: EdgeInsets.only(left: deviceWidth * 0.03),
                       child: Text("Shubham"),
                     ),
                   ),
@@ -197,7 +205,7 @@ class _CricketTeamDetasilsInputState extends State<CricketTeamDetasilsInput> {
                   Expanded(
                     flex: 1,
                     child: Container(
-                      margin: EdgeInsets.only(left: 15.0),
+                      margin: EdgeInsets.only(left: deviceWidth * 0.03),
                       child: Text("₹15,000"),
                     ),
                   ),
@@ -207,25 +215,28 @@ class _CricketTeamDetasilsInputState extends State<CricketTeamDetasilsInput> {
                 width: MediaQuery.of(context).size.width,
                 height: MediaQuery.of(context).size.height -
                     MediaQuery.of(context).size.height / 2.5,
-                margin: EdgeInsets.fromLTRB(10, 50, 10, 10),
+                margin: EdgeInsets.fromLTRB(deviceWidth * 0.02,
+                    deviceWidth * 0.1, deviceWidth * 0.02, deviceWidth * 0.02),
                 child: Card(
                   elevation: 10,
                   shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.0)),
+                      borderRadius: BorderRadius.circular(deviceWidth * 0.03)),
                   color: Colors.white.withOpacity(0.2),
                   child: Column(
                     children: [
                       SizedBox(
-                        height: 40,
+                        height: deviceWidth * 0.08,
                       ),
                       Expanded(
                         flex: 2,
                         child: Container(
                           width: MediaQuery.of(context).size.width,
-                          height: 50,
-                          margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                          height: deviceWidth * 0.1,
+                          margin: EdgeInsets.fromLTRB(deviceWidth * 0.02,
+                              deviceWidth * 0.02, deviceWidth * 0.02, 0),
                           decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
+                              borderRadius:
+                                  BorderRadius.circular(deviceWidth * 0.02),
                               color: Colors.black.withOpacity(0.4)),
                           child: Row(
                             children: [
@@ -241,16 +252,17 @@ class _CricketTeamDetasilsInputState extends State<CricketTeamDetasilsInput> {
                               Expanded(
                                 flex: 1,
                                 child: Container(
-                                  width: 42,
-                                  height: 35,
-                                  margin: EdgeInsets.fromLTRB(40, 0, 0, 0),
+                                  width: deviceWidth * 0.09,
+                                  height: deviceWidth * 0.07,
+                                  margin: EdgeInsets.fromLTRB(
+                                      deviceWidth * 0.08, 0, 0, 0),
                                   child: FlatButton(
                                     onPressed: () {
                                       showDialog(
                                           context: context,
                                           builder: (BuildContext context) =>
-                                              leaddialog(
-                                                  context, "Team A", 11));
+                                              leaddialog(context, "Team A", 11,
+                                                  deviceWidth));
                                     },
                                     child: Image.asset(
                                       "assets/edit_button.png",
@@ -273,10 +285,12 @@ class _CricketTeamDetasilsInputState extends State<CricketTeamDetasilsInput> {
                         flex: 2,
                         child: Container(
                           width: MediaQuery.of(context).size.width,
-                          height: 50,
-                          margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                          height: deviceWidth * 0.1,
+                          margin: EdgeInsets.fromLTRB(deviceWidth * 0.02,
+                              deviceWidth * 0.02, deviceWidth * 0.02, 0),
                           decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
+                              borderRadius:
+                                  BorderRadius.circular(deviceWidth * 0.02),
                               color: Colors.black.withOpacity(0.4)),
                           child: Row(
                             children: [
@@ -292,16 +306,17 @@ class _CricketTeamDetasilsInputState extends State<CricketTeamDetasilsInput> {
                               Expanded(
                                 flex: 1,
                                 child: Container(
-                                  width: 42,
-                                  height: 35,
-                                  margin: EdgeInsets.fromLTRB(40, 0, 0, 0),
+                                  width: deviceWidth * 0.09,
+                                  height: deviceWidth * 0.07,
+                                  margin: EdgeInsets.fromLTRB(
+                                      deviceWidth * 0.08, 0, 0, 0),
                                   child: FlatButton(
                                     onPressed: () {
                                       showDialog(
                                           context: context,
                                           builder: (BuildContext context) =>
-                                              leaddialog(
-                                                  context, "Team B", 11));
+                                              leaddialog(context, "Team B", 11,
+                                                  deviceWidth));
                                     },
                                     child: Image.asset(
                                       "assets/edit_button.png",
@@ -315,11 +330,11 @@ class _CricketTeamDetasilsInputState extends State<CricketTeamDetasilsInput> {
                         ),
                       ),
                       SizedBox(
-                        height: 70,
+                        height: deviceWidth * 0.16,
                       ),
                       Container(
                         width: MediaQuery.of(context).size.width,
-                        height: 45,
+                        height: deviceWidth * 0.15,
                         child: RaisedButton(
                           onPressed: () {
                             Navigator.push(
@@ -345,7 +360,8 @@ class _CricketTeamDetasilsInputState extends State<CricketTeamDetasilsInput> {
     );
   }
 
-  Dialog leaddialog(BuildContext context, String team_name, int team_size) =>
+  Dialog leaddialog(BuildContext context, String team_name, int team_size,
+          double deviceWidth) =>
       Dialog(
         child: Container(
           width: MediaQuery.of(context).size.width,
